6d978eeaad9f29af3507359ce61cdf05e8d48721
Mandaline <mandy@wearecapicua.com>
2024-09-17 13:37:46 -0300

    fix routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,29 @@
 import { Menu } from 'antd';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import { WechatOutlined, EditOutlined } from '@ant-design/icons';
 
 const Header = () => {
-  const [current, setCurrent] = useState('h');
+  const location = useLocation(); 
+  const [current, setCurrent] = useState(location.pathname === '/products' ? 'r' : 'h');
+
   const onClick = (e: any) => {
     console.log('click ', e);
     setCurrent(e.key);
   };
+
   return (
     <>
-     <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" theme="dark" >
-      <Menu.Item key="h" icon= {<WechatOutlined />}>
-       <Link to="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item key="r" icon= {<EditOutlined />}>
-        <Link to="/products">Products</Link>
-      </Menu.Item>
-     </Menu>
-     <Outlet/>
+      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" theme="dark" >
+        <Menu.Item key="h" icon= {<WechatOutlined />}>
+          <Link to="/">Chat</Link>
+        </Menu.Item>
+        <Menu.Item key="r" icon= {<EditOutlined />}>
+          <Link to="/products">Products</Link>
+        </Menu.Item>
+      </Menu>
+      <Outlet />
     </>
-   
   )
 };
 export default Header;
